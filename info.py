1cdf1ee705e10fa304a0ac1c3d5e2c88343835d0
Ddosjsjhssh <chetanmehar588@gmail.com>
2025-12-20 00:30:46 +0530

    Update DATABASE_URI and DATABASE_URI2 values

--- info.py
+++ info.py
@@ -42,13 +42,13 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002517228726') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://chetan:chetan1@cluster0.agvz0cd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Ludo:RpfS4DiD5eXvt4dz@cluster0.3zphwiq.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "mehar")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mehar')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://chetan2:chetan2@cluster0.urotykv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 DB_CHANGE_LIMIT = int(environ.get('DB_CHANGE_LIMIT', "256")) 
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_hd_hub15')
